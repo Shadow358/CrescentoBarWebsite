ec4919796cd47a31a69b048aa86f1169b4a81e55
Shadow358 <shadow358@abv.bg>
2014-10-16 20:53:31 +0300

    Final

---
+++ contactthanks.php
@@ -0,0 +1,28 @@
+
+
+<head>
+	<title>Thank You!</title>
+	
+</head>
+
+<body>
+
+	
+
+		
+		
+			
+			
+		<br /><br />
+		
+			
+		<h1>Your message has been sent! Thank You!</h1><br />
+		
+		<p><a href="index.html">Back to our page</a></p>
+	
+	
+	
+
+</body>
+
+</html>
